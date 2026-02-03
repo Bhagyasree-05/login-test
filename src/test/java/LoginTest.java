3e02c8862b67a18c449de857f84450126dc4c40c
Bhagya <bhagyasreepalaka@gmail.com>
2026-02-03 21:45:11 +0530

    initial project

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,16 +1,20 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.time.Duration;
+import java.io.File;
 
 public class LoginTest {
 
     @Test
     public void loginAutomationTest() throws Exception {
 
-        // ChromeDriver automatic setup
+        // Setup ChromeDriver automatically
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
@@ -21,17 +25,21 @@ public class LoginTest {
 
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("file:///C:/Users/LENOVO/Desktop/login-test/src/test/resources/login.html");
+        // Use relative path
+        String loginPage = new File("src/test/resources/login.html").getAbsolutePath();
+        driver.get("file:///" + loginPage.replace("\\", "/"));
 
-        // Automation input - change anytime
+        // Automation input - change email/password anytime
         driver.findElement(By.id("username")).sendKeys("bhagyasree2@gmail.com");
         driver.findElement(By.id("password")).sendKeys("Strong@123");
         driver.findElement(By.tagName("button")).click();
 
-        Thread.sleep(2000); // wait for message update
+        // Wait until message appears (max 5 sec)
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement msg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("msg")));
 
-        String msg = driver.findElement(By.id("msg")).getText();
-        Assert.assertEquals(msg, "Login Successful");
+        String text = msg.getText();
+        Assert.assertEquals(text, "Login Successful");
 
         driver.quit();
     }

da19dfdbfd7810ad62148b7caad56f00a924180c
Bhagya <bhagyasreepalaka@gmail.com>
2026-02-03 21:47:15 +0530

    initial project

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -14,27 +14,25 @@ public class LoginTest {
     @Test
     public void loginAutomationTest() throws Exception {
 
-        // Setup ChromeDriver automatically
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // Jenkins safe
+        options.addArguments("--headless=new");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
 
         WebDriver driver = new ChromeDriver(options);
 
-        // Use relative path
-        String loginPage = new File("src/test/resources/login.html").getAbsolutePath();
-        driver.get("file:///" + loginPage.replace("\\", "/"));
+        // ✅ Correct way to load local HTML page
+        File loginFile = new File("src/test/resources/login.html");
+        String loginPath = loginFile.getAbsolutePath().replace("\\", "/");
+        driver.get("file:///" + loginPath);
 
-        // Automation input - change email/password anytime
         driver.findElement(By.id("username")).sendKeys("bhagyasree2@gmail.com");
         driver.findElement(By.id("password")).sendKeys("Strong@123");
         driver.findElement(By.tagName("button")).click();
 
-        // Wait until message appears (max 5 sec)
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         WebElement msg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("msg")));
 
